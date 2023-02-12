8adfb33be266872ca56694395dcdd7d72d3f6c3d
Fuglesteg <andreasfdale@gmail.com>
2023-02-12 23:17:55 +0000

    Add ContainerIsPaused method

--- docker/dockerController.go
+++ docker/dockerController.go
@@ -24,51 +24,59 @@ func NewDockerController() *DockerController {
 }
 
 func (controller *DockerController) StopContainer(container *Container) {
-    err := controller.client.ContainerKill(context.Background(), container.ID, "SIGTERM")   
-    if err != nil {
-        verboseLog.Checkreport(1, err)
-    }
+	err := controller.client.ContainerKill(context.Background(), container.ID, "SIGTERM")
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
 }
 
 func (controller *DockerController) PauseContainer(container *Container) {
-    err := controller.client.ContainerPause(context.Background(), container.ID)
-    if err != nil {
-        verboseLog.Checkreport(1, err)
-    }
+	err := controller.client.ContainerPause(context.Background(), container.ID)
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
 }
 
 func (controller *DockerController) UnpauseContainer(container *Container) {
-    err := controller.client.ContainerUnpause(context.Background(), container.ID) 
-    if err != nil {
-        verboseLog.Checkreport(1, err)
-    }
+	err := controller.client.ContainerUnpause(context.Background(), container.ID)
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
 }
 
 func (controller *DockerController) StartContainer(container *Container) {
-    err := controller.client.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
-    if err != nil {
-        verboseLog.Checkreport(1, err)
-    }
+	err := controller.client.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
 }
 
-func (controller *DockerController) ContainerIsRunning(container *Container) bool{
-    info, err := controller.client.ContainerInspect(context.Background(), container.ID)    
-    if err != nil {
-        verboseLog.Checkreport(1, err)
-    }
-    return info.State.Running
+func (controller *DockerController) ContainerIsRunning(container *Container) bool {
+	info, err := controller.client.ContainerInspect(context.Background(), container.ID)
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
+	return info.State.Running
+}
+
+func (controller *DockerController) ContainerIsPaused(container *Container) bool {
+	info, err := controller.client.ContainerInspect(context.Background(), container.ID)
+	if err != nil {
+		verboseLog.Checkreport(1, err)
+	}
+	return info.State.Paused
 }
 
 func (controller *DockerController) NewContainer(containerName string) (*Container, error) {
-    filterArgs := filters.NewArgs(
-        filters.Arg("name", containerName),
-    )
-    listOptions := types.ContainerListOptions{All: true, Filters: filterArgs}
-    containers, err := 
-        controller.client.ContainerList(context.Background(), listOptions)
-    if err != nil {
-        return nil, err
-    }
-    container := &Container{Name: containerName, ID: containers[0].ID}
-    return container, nil
+	filterArgs := filters.NewArgs(
+		filters.Arg("name", containerName),
+	)
+	listOptions := types.ContainerListOptions{All: true, Filters: filterArgs}
+	containers, err :=
+		controller.client.ContainerList(context.Background(), listOptions)
+	if err != nil {
+		return nil, err
+	}
+	container := &Container{Name: containerName, ID: containers[0].ID}
+	return container, nil
 }
